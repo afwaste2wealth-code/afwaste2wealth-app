7dd82053923cac3fff98db7e5ee36061fad4493e
afwaste2wealth-code <afwaste2wealth@gmail.com>
2026-09-01 06:36:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ recordMaterialIn();
     name +
       " module selected. This prototype is ready to be connected to the A&F data and backend."
   );
-}
+} 
  
  
 /* =========================================================
